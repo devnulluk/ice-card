ba142f12038d7eef87353cbe5239c6a5bf57479a
devnulluk <mark@devnull.co.uk>
2025-07-03 10:17:28 +0100

    Created basic webpage to appear when the card is scanned Using environment variables for details until I setup a database

--- id1.php
+++ id1.php
@@ -8,14 +8,30 @@ use Ntfy\Server;
 use Ntfy\Message;
 use Ntfy\Client;
 
-$server = new Server('$_ENV['ntfy_server_url']');
+$server = new Server($_ENV['ntfy_server_url']);
 
 $message = new Message();
-$message->topic('$_ENV['ntfy_topic']');
-$message->title('Card Scanned');
+$message->topic($_ENV['ntfy_topic']);
+$message->title($_ENV['card_name'] . " Card Scanned");
 $message->body('An ICE card has been scanned successfully.');
 $message->priority(Message::PRIORITY_MAX);
 
 $client = new Client($server);
 $client->send($message);
+
 ?>
+<html>
+<head>
+    <title><?php echo $_ENV['card_name']; ?> - ICE</title>
+    <link rel="stylesheet" href="stylesheet.css" type="text/css" charset="utf-8" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+</head>
+<body>
+    <h1><?php echo $_ENV['card_name']; ?> - ICE</h1>
+    <p>In case of emergancy please contact:</p>
+    <p><?php echo $_ENV['ice_name']; ?> <a href="tel:<?php echo $_ENV['ice_tel']; ?>"><?php echo $_ENV['ice_tel']; ?></a></p>
+</body>
+</html>
